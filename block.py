c9309ec937e726253fd83b2b99906799e2d73d15
JamesAndrew <jandrewshields@gmail.com>
2018-04-28 14:11:57 -0600

    a

--- block.py
+++ block.py
@@ -1,5 +1,7 @@
+import boto3
 import hashlib
 
+# define object Block with attributes miner name, block number n, nonce, data, hash h, and previous hash
 class Block:
     miner = ''
     n = 0
@@ -8,25 +10,40 @@ class Block:
     h = 0
     prevh = 0
 
-    def __init__(self, miner, n, nonce, data, prevh):
+    def __init__(self, miner, n, data, prevh):
         print('new block attempt')
+        self.miner = miner
+        self.n = n
+        self.data = data
+        self.prevh = prevh
+
+    # create local copy of a block from another miner
+    def otherBlock(self, miner, n, nonce, data, h, prevh):
         self.miner = miner
         self.n = n
         self.nonce = nonce
         self.data = data
+        self.h = h
         self.prevh = prevh
 
     def display(self):
         print('Peer ' + self.miner)
         print('# ' + str(self.n) + ", nonce = " + str(self.nonce))
         print(self.data)
+        print('hash = ' + self.h.hexdigest())
         print('prevhash = ' + str(self.prevh))
 
     def mine(self):
         print('mining started')
-        hashV = self.miner + str(self.n) + str(self.nonce) + str(self.data) + str(self.prevh)
-        print(hashV)
-        
-        self.h = hashlib.sha256()
-        self.h.update(hashV.encode('UTF-8'))
-        print(self.h.hexdigest())
+
+        # is block being mined
+        mining = True
+        while(mining):
+            hashV = self.miner + str(self.n) + str(self.nonce) + str(self.data) + str(self.prevh)
+            self.h = hashlib.sha256()
+            self.h.update(hashV.encode('UTF-8'))
+            s = self.h.hexdigest()
+            if s[0] == '0' and s[1] == '0' and s[2] == '0' and s[3] == '0':
+                mining = False
+            else:
+                self.nonce = self.nonce + 1        
--- miner.py
+++ miner.py
@@ -1,19 +1,37 @@
 import boto3
 import block
 
+sqs = boto3.resource('sqs')
+
 miner = input("miner name")
 
-# create initial block if it doesn't exist
-b = block.Block(miner, 0, 0, "", 0)
-b.mine()
-chain = []
-chain.append(b)
+# give miner 100 coins to start with
+coinbase = 100
+
+# create initial block if miner a
+if (miner == 'a'):
+    b = block.Block(miner, 0, "", 0)
+    b.mine()
+    chain = []
+    chain.append(b)
+
+    # send block to everyone
+    sqs.get_queue_by_name(QueueName='nodeb').send_message(MessageBody="add," + b.miner + ',' \
+        + str(b.n) + ',' + str(b.nonce) + ',' + str(b.data) + ',' + str(b.h) + ',' + str(b.prevh))
+
+def checkMsgs():
+    msg = sqs.get_queue_by_name(QueueName='node' + miner).receive_messages()
+    m = msg[0].split(",")
+    if m[0] == 'add'
+        c = block.Block(miner, 0, "", 0)
+        c.otherBlock(m[1], m[2], m[3], m[4], m[5], m[6])
+        chain.append(c)
 
 # main loop
 loop = True
 while loop:
     print("BLOCKCHAIN")
-    print("options - (0) quit, (1) mine, (2) display blockchain")
+    print("options - (0) quit, (1) mine, (2) display blockchain, (3) check messages")
     option = input("option?")
     if option == "0":
         loop = False
@@ -23,5 +41,8 @@ while loop:
         print("displaying blockchain")
         for item in chain[:]:
             item.display()
+    elif option == "3":
+        print('checking messages')
+        checkMsgs()
     else:
         print("not a valid option")

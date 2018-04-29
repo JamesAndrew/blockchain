a9c862f9ea6de69d90b382fb3de4811e5d61780c
JamesAndrew <jandrewshields@gmail.com>
2018-04-29 14:27:58 -0600

    jhv

--- miner.py
+++ miner.py
@@ -8,7 +8,7 @@ miner = input("miner name")
 chain = []
 
 def sendMsgs(m):
-    for char in 'abcd':
+    for char in 'ab':
         if char != miner:
             sqs.get_queue_by_name(QueueName='node' + char).send_message(MessageBody=m)
 
